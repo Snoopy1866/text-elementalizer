b1db63128725f9f260ad3ccb40706d6de2b500c7
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-08-16 14:42:44 +0800

    fix: press modifier keys cause result text update #19

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import PySimpleGUI as psg
 from elementalizer import elementalizer
 import icon
 
+# 事件-更新转换结果
+def txt_result_update(source, ignore_tone = True):
+    if ignore_tone:
+        result = elementalizer(source, ignore_tone = True)
+    else:
+        result = elementalizer(source, ignore_tone = False)
+
+    window['txt_result'].update(result)
+
+
+# 界面布局
 layout = [
     [psg.Text('请输入需要转换的文本：'),
      psg.Input(key = 'txt_source', expand_x = True, enable_events = True)],
@@ -16,6 +27,7 @@ layout = [
 ]
 
 window = psg.Window('化学名称文本转换器', layout, icon = icon.icon)
+last_source = ''
 while True:
     event, values = window.read()
 
@@ -29,13 +41,12 @@ while True:
         window['txt_copy_hint'].update('已复制到剪贴板', visible = True)
 
     # 转换文本
-    if event in ['txt_source', 'chk_ignore_tone', 'btn_regenerate']:
-        if values['chk_ignore_tone']:
-            result = elementalizer(values['txt_source'], ignore_tone = True)
-        else:
-            result = elementalizer(values['txt_source'], ignore_tone = False)
-
-    # 更新结果
-    window['txt_result'].update(result)
+    if event in ['txt_source']:
+        current_source = values['txt_source']
+        if current_source != last_source:
+            txt_result_update(current_source, ignore_tone = values['chk_ignore_tone'])
+            last_source = current_source
+    elif event in ['chk_ignore_tone', 'btn_regenerate']:
+        txt_result_update(current_source, ignore_tone = values['chk_ignore_tone'])
 
 window.close()
